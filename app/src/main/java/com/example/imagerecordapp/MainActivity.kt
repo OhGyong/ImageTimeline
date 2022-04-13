60baf37fbb193bedb564a7c855ff1164d75d8158
OhGyong <oky7143@naver.com>
2022-04-13 23:08:49 +0900

    콘텐트 프로바이더 함수 사용(contentResolver), cursor를 통한 DB 데이터 처리를 통해 이미지의 날짜 정보 가져오기 성공

--- app/src/main/java/com/example/imagerecordapp/MainActivity.kt
+++ app/src/main/java/com/example/imagerecordapp/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.imagerecordapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.database.Cursor
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import java.io.File
+import java.sql.Date
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var image: ImageView
+
+
+    @SuppressLint("Range")
+    @RequiresApi(Build.VERSION_CODES.S)
+    private val resultListener =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+            var cursor: Cursor = contentResolver.query(it.data!!.data!!,null,null,null,null)!!
+            cursor.moveToFirst()
+            var path = cursor.getLong(3) // 날짜 정보
+
+            var date = Date((path)) // 날짜 타입으로 변환
+            println("날짜 : $date")
+
+            try{
+                image.setImageURI(it.data?.data)
+            }catch (error: Error){
+                println("에러")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.image_gallery)
+        val imageBtn = findViewById<ImageButton>(R.id.image_record)
+
+        imageBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            resultListener.launch(intent)
+        }
     }
 }
