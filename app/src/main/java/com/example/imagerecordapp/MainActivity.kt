15c911556fc3c61a859e7d459418d5ee58ded6f6
OhGyong <oky7143@naver.com>
2022-04-19 23:30:56 +0900

    registerForActivityResult 했을 때 권한 허용 추가

--- app/src/main/java/com/example/imagerecordapp/MainActivity.kt
+++ app/src/main/java/com/example/imagerecordapp/MainActivity.kt
@@ -3,21 +3,15 @@ package com.example.imagerecordapp
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.database.Cursor
-import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.example.imagerecordapp.data.GridViewData
 import com.example.imagerecordapp.data.GridViewDatabase
-import com.example.imagerecordapp.data.ViewData
 import com.example.imagerecordapp.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -26,32 +20,29 @@ import java.sql.Date
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-//    val itemList = arrayListOf<GridViewData>()
-    val itemList = ArrayList<ViewData>()
-
-//    private lateinit var image: ImageView
-
     private lateinit var db: GridViewDatabase
-
 
     @SuppressLint("Range")
     @RequiresApi(Build.VERSION_CODES.S)
     private val resultListener =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-
-            val cursor: Cursor = contentResolver.query(it.data!!.data!!,null,null,null,null)!!
-            cursor.moveToFirst()
-            val path = cursor.getLong(3) // 날짜 정보
-
-            val date = Date(path) // 날짜 타입으로 변환
-            println("날짜 : $date")
-
-            GlobalScope.launch(Dispatchers.IO){
-                db.gridViewDao().insertData(GridViewData(date.toString(), it.data!!.data!!.toString()))
-            }
-
             try{
-//                image.setImageURI(it.data?.data)
+                val uri = it.data!!.data!!
+
+                // 권한 허용으로 에러 처리.
+                contentResolver.takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                contentResolver.takePersistableUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+
+                val cursor: Cursor = contentResolver.query(uri,null,null,null,null)!!
+                cursor.moveToFirst()
+                val path = cursor.getLong(3) // 날짜 정보
+
+                val date = Date(path) // 날짜 타입으로 변환
+                println("날짜 : $date")
+
+                GlobalScope.launch(Dispatchers.IO){
+                    db.gridViewDao().insertData(GridViewData(date.toString(), uri.toString())) // 데이터 삽입
+                }
             }catch (error: Error){
                 println("에러")
             }
@@ -69,17 +60,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         GlobalScope.launch(Dispatchers.IO){
-            for(i in db.gridViewDao().getAll()){
-                itemList.add(ViewData(i.date, Uri.parse(i.imgUri)))
-                println(itemList)
-            }
-            binding.viewGrid.adapter = MainGridAdapter(itemList)
+            binding.viewGrid.adapter = MainGridAdapter(db.gridViewDao().getAll())
         }
 
-//        image = findViewById(R.id.image_gallery)
-
         binding.imageRecord.setOnClickListener {
-            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
             intent.type = "image/*"
             resultListener.launch(intent)
         }
