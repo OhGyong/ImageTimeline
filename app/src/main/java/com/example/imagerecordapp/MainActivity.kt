3a870027b891834a7a4ccb406f8f0e4ecf5aa543
OhGyong <oky7143@naver.com>
2022-04-20 00:09:24 +0900

    ViewModel 추가해서 코드 수정

--- app/src/main/java/com/example/imagerecordapp/MainActivity.kt
+++ app/src/main/java/com/example/imagerecordapp/MainActivity.kt
@@ -8,19 +8,23 @@ import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.example.imagerecordapp.data.GridViewData
 import com.example.imagerecordapp.data.GridViewDatabase
 import com.example.imagerecordapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.sql.Date
 
+@DelicateCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var db: GridViewDatabase
+    private lateinit var viewModel: MainViewModel
 
     @SuppressLint("Range")
     @RequiresApi(Build.VERSION_CODES.S)
@@ -52,21 +56,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // 바인딩 초기화
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 뷰모델 초기화
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        // room의 db 초기화
         db = Room.databaseBuilder(
             applicationContext, GridViewDatabase::class.java, "database"
         ).build()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        GlobalScope.launch(Dispatchers.IO){
-            binding.viewGrid.adapter = MainGridAdapter(db.gridViewDao().getAll())
-        }
+        viewModel.imageList.observe(this, {
+            binding.viewGrid.adapter = MainGridAdapter(it)
+        })
 
         binding.imageRecord.setOnClickListener {
             val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
             intent.type = "image/*"
             resultListener.launch(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.getImageListData(db)
     }
 }
