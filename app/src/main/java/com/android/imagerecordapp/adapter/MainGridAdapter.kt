c3569e414db36d3ac07f379897bf9ec34e853715
OhGyong <ogyong7143@gmail.com>
2023-01-02 23:37:43 +0900

    코드 정리

--- app/src/main/java/com/android/imagerecordapp/adapter/MainGridAdapter.kt
+++ app/src/main/java/com/android/imagerecordapp/adapter/MainGridAdapter.kt
@@ -8,24 +8,24 @@ import com.android.imagerecordapp.data.GridViewData
 import com.android.imagerecordapp.databinding.GridViewItemBinding
 
 class MainGridAdapter : RecyclerView.Adapter<MainGridAdapter.MainGridViewHolder>() {
-
-    private var list = ArrayList<GridViewData>()
-
     // Activity에서 사용하기 위한 클릭 리스너
     interface OnItemClickListener{
         fun onItemClick(v: View, data: GridViewData, pos: Int)
     }
 
+    private var list = ArrayList<GridViewData>()
+
     private var listener: OnItemClickListener? = null
+
     fun setOnItemClickListener(listener: OnItemClickListener){
         this.listener = listener
     }
 
-    inner class MainGridViewHolder(private val binding: GridViewItemBinding) :
-        RecyclerView.ViewHolder(binding.root){
+    inner class MainGridViewHolder(private val mBinding: GridViewItemBinding) :
+        RecyclerView.ViewHolder(mBinding.root){
 
         fun bind(viewData: GridViewData) {
-            binding.gridViewItem = viewData
+            mBinding.gridViewItem = viewData
 
               if(adapterPosition != RecyclerView.NO_POSITION){
                 itemView.setOnClickListener {
