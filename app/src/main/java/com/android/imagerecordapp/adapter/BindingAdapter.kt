8dad4e0b32883d08258babe603479a7961cd36bc
O_Gyong <ogyong7143@gmail.com>
2023-01-30 16:46:28 +0900

    이미지 라운드 처리 적용

--- app/src/main/java/com/android/imagerecordapp/adapter/BindingAdapter.kt
+++ app/src/main/java/com/android/imagerecordapp/adapter/BindingAdapter.kt
@@ -4,6 +4,8 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.android.imagerecordapp.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 object BindingAdapter {
     @BindingAdapter("imageUrl")
@@ -11,6 +13,7 @@ object BindingAdapter {
     fun bindImage(imgView: ImageView, imgUrl: String?){
         Glide.with(imgView.context)
             .load(imgUrl)
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(14)))
             .error(R.drawable.error)
             .into(imgView)
     }
