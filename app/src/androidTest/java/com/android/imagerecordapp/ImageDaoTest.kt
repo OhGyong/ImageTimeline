2598354fa2dcf18aedf9a065ab3f6df7844bb558
OhGyong <ogyong7143@gmail.com>
2023-04-05 23:07:19 +0900

    Junit으로 Room insert 테스트

---
+++ app/src/androidTest/java/com/android/imagerecordapp/ImageDaoTest.kt
@@ -0,0 +1,46 @@
+package com.android.imagerecordapp
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.android.imagerecordapp.data.ImageViewDao
+import com.android.imagerecordapp.data.ImageViewData
+import com.android.imagerecordapp.data.ImageViewDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class ImageDaoTest {
+    private lateinit var imageDb: ImageViewDatabase
+    private lateinit var imageDao: ImageViewDao
+
+    @Before
+    fun openDb() {
+        imageDb = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            ImageViewDatabase::class.java
+        ).allowMainThreadQueries().build()
+        imageDao = imageDb.imageViewDao()
+    }
+
+    @After
+    fun closeDb() {
+        imageDb.close()
+    }
+
+    @Test
+    fun test() = runBlocking {
+        val imageData = ImageViewData("2022-12-12", "uri")
+        CoroutineScope(Dispatchers.IO).launch {
+            imageDao.insertData(imageData)
+        }.join()
+    }
+}
