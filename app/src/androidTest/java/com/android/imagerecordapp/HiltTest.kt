b871e5d02f44f6f3df8d53921e30699988ae18c1
OhGyong <ogyong7143@gmail.com>
2023-04-05 17:43:43 +0900

    CustomTestRunner 파일 삭제, 해당 클래스 HiltTest로 이동

--- app/src/androidTest/java/com/android/imagerecordapp/HiltTest.kt
+++ app/src/androidTest/java/com/android/imagerecordapp/HiltTest.kt
@@ -1,12 +1,21 @@
 package com.android.imagerecordapp
 
+import android.app.Application
+import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+class CustomTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
 class HiltTest {
