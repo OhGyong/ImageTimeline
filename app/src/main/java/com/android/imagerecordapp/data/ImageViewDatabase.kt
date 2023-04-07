d9591cac7e0331d9337e42d09eff0d5952eab059
OhGyong <ogyong7143@gmail.com>
2023-04-07 17:18:41 +0900

    Room Database exportSchema 값 false 처리

--- app/src/main/java/com/android/imagerecordapp/data/ImageViewDatabase.kt
+++ app/src/main/java/com/android/imagerecordapp/data/ImageViewDatabase.kt
@@ -3,7 +3,7 @@ package com.android.imagerecordapp.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ImageViewData::class], version = 1)
+@Database(entities = [ImageViewData::class], version = 1, exportSchema = false)
 abstract class ImageViewDatabase : RoomDatabase() {
     abstract fun imageViewDao() : ImageViewDao
 }
