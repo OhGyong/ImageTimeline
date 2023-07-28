defa7a0e0faf49c6b69c697ce7d8b41ed4c13962
O_Gyong <ogyong7143@gmail.com>
2023-07-28 17:57:44 +0900

    diskCacheStrategy All 적용(gif 허용)

--- app/src/main/java/com/android/imagerecordapp/adapter/BindingAdapter.kt
+++ app/src/main/java/com/android/imagerecordapp/adapter/BindingAdapter.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.android.imagerecordapp.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 
@@ -15,6 +16,7 @@ object BindingAdapter {
             .load(imgUrl)
             .apply(RequestOptions.bitmapTransform(RoundedCorners(14)))
             .error(R.drawable.error)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(imgView)
     }
 }
